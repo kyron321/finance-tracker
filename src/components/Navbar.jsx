2b3a56f1c9548b531280e98605204078e822bb8d
Kyron Smith <kds060902@outlook.com>
2023-01-14 18:53:52 +0000

    feat: add conditional rendering based on user state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,38 @@
-import { Link } from "react-router-dom"
-import {useLogout} from "../hooks/useLogout"
-import styles from "./Navbar.module.css"
+import { Link } from "react-router-dom";
+import { useLogout } from "../hooks/useLogout";
+import styles from "./Navbar.module.css";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 export default function Navbar() {
-  const {logout} =useLogout()
+  const { logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <nav className={styles.navbar}>
-        <ul>
-            <li className={styles.title}>Piggy Bank</li>
-            <li><Link to="/login">Login</Link></li>
-            <li><Link to="/signup">Signup</Link></li>
+      <ul>
+        <li className={styles.title}>Piggy Bank</li>
+        {!user && (
+          <>
             <li>
-              <button className="btn" onClick={logout}>
-                Logout
-              </button>
+              <Link to="/login">Login</Link>
             </li>
-        </ul>
+            <li>
+              <Link to="/signup">Signup</Link>
+            </li>
+          </>
+        )}
+
+        {user && (
+          <>
+          <li>hello, {user.displayName}</li>
+          <li>
+            <button className="btn" onClick={logout}>
+              Logout
+            </button>
+          </li>
+          </>
+        )}
+      </ul>
     </nav>
-  )
+  );
 }
