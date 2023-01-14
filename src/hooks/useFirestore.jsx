fb5dc0edc9d21790ba1cda70a42b0dcb05447312
Kyron Smith <kds060902@outlook.com>
2023-01-14 19:57:44 +0000

    feat: add firestore documents

--- src/hooks/useFirestore.jsx
+++ src/hooks/useFirestore.jsx
@@ -10,6 +10,22 @@ let initialState = {
 
 const firestoreReducer = (state, action) => {
   switch (action.type) {
+    case "IS_PENDNING":
+      return { isPending: true , document:null, success:false, error:null};
+    case "ADDED_DOCUMENT":
+      return {
+        isPending: false,
+        document: action.payload,
+        success: true,
+        error: null,
+      };
+    case "ERROR":
+      return {
+        isPending: false,
+        document: null,
+        success: false,
+        error: action.payload,
+      };
     default:
       return state;
   }
@@ -17,23 +33,37 @@ const firestoreReducer = (state, action) => {
 
 export const useFirestore = (collection) => {
   const [response, dispatch] = useReducer(firestoreReducer, initialState);
-  const [isCancelled, setIsCancelled] = useState(false)
+  const [isCancelled, setIsCancelled] = useState(false);
 
   //collection ref
-  const ref = projectFirestore.collection(collection)
+  const ref = projectFirestore.collection(collection);
+
+  //only dispatch if not canceleed
+  const dispatchIfNotCancelled = (action) => {
+    if (!isCancelled) {
+      dispatch(action);
+    }
+  };
+
   //add document
-  const addDocument = (doc)=>{
-
-  }
+  const addDocument = async (doc) => {
+    dispatch({ type: "IS_PENDING" });
+    try {
+      const addedDocument = await ref.add({ doc });
+      dispatchIfNotCancelled({
+        type: "ADDED_DOCUMENT",
+        payload: addedDocument,
+      });
+    } catch (err) {
+      dispatchIfNotCancelled({ type: "ERROR", payload: err.message });
+    }
+  };
   //delete document
-  const deleteDocument=(id)=>{
+  const deleteDocument = async (id) => {};
 
-  }
+  useEffect(() => {
+    return () => setIsCancelled(true);
+  }, []);
 
-useEffect(()=>{
-    return ()=> setIsCancelled(true)
-},[])
-
-return {addDocument, deleteDocument, response}
-
+  return { addDocument, deleteDocument, response };
 };
