d863ffb55cd89c8a1efb132fa4970dc3a1d9ca3a
Kyron Smith <kds060902@outlook.com>
2023-01-14 17:13:59 +0000

    feat: setup firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import firebase from "firebase/app";
+import "firebase/firestore"
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDToJZayBJRdbyRVXA5z_FzHEA-19Yx6cg",
+    authDomain: "finance-tracker-b3f57.firebaseapp.com",
+    projectId: "finance-tracker-b3f57",
+    storageBucket: "finance-tracker-b3f57.appspot.com",
+    messagingSenderId: "220998183849",
+    appId: "1:220998183849:web:84f439bb6dc15648d8b2b8",
+    measurementId: "G-LSEQ28QC3B"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+  const projectFirestore = firebase.firestore()
+
+  export {projectFirestore}
