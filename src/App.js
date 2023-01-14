93ef490e18a4ebe1fd47d36bb6e485c690f51ba4
Kyron Smith <kds060902@outlook.com>
2023-01-14 19:13:43 +0000

    feat: add conditional rendering to wait until user state check is complete

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Signup from "./pages/signup/Signup";
 import Navbar from "./components/Navbar"
+import { useAuthContext } from "./hooks/useAuthContext";
 
 function App() {
+  const {authIsReady} = useAuthContext()
+
   return (
     <div className="App">
+      {authIsReady && (
       <BrowserRouter>
       <Navbar/>
         <Switch>
@@ -21,6 +25,7 @@ function App() {
           </Route>
         </Switch>
       </BrowserRouter>
+      )}
     </div>
   );
 }
