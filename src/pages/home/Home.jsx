0f649f103cb01f6b8aeb91e923f8ba70aad9f35a
Kyron Smith <kds060902@outlook.com>
2023-01-14 19:34:21 +0000

    feat: add basic transaction form

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,15 @@
 import styles from "./Home.module.css"
+import TransactionForm from "./TransactionForm"
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.container}>
+      <div className={styles.content}>
+        transaction list
+      </div>
+      <div className={styles.sidebar}>
+        <TransactionForm/>
+      </div>
+    </div>
   )
 }
